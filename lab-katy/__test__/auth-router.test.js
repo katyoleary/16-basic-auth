ea21f2843646a6db1d4f5fb51b3b37abfdf14385
Katy Robinson <katyorob@gmail.com>
2018-03-06 15:44:16 -0800

    fixed linter errors

--- lab-katy/__test__/auth-router.test.js
+++ lab-katy/__test__/auth-router.test.js
@@ -2,8 +2,6 @@
 
 //after were done, start making apps with superagent instead of express.
 const request = require('superagent');
-const mongoose = require('mongoose');
-const Promise = require('bluebird');
 const User = require('../model/user.js');
 const serverToggle = require('../lib/server-toggle.js');
 const server = require('../server.js');
@@ -15,8 +13,8 @@ const url = 'http://localhost:3000';
 const exampleUser = {
   username: 'exampleuser', 
   password: '1234',
-  email: 'exampleuser@test.com'
-}
+  email: 'exampleuser@test.com',
+};
 
 describe('Authorization Routes', function() {
 
@@ -36,32 +34,32 @@ describe('Authorization Routes', function() {
 
       afterEach( done => {
         User.remove({})
-        .then( () => done())
-        .catch(done);
+          .then( () => done())
+          .catch(done);
       });
 
       it('should return a token', done => {
         request.post(`${url}/api/signup`)
-        .send(exampleUser)
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).toEqual(200);
-          expect(typeof res.text).toEqual('string');
-          done();
-        });
+          .send(exampleUser)
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(typeof res.text).toEqual('string');
+            done();
+          });
       });
     });
 
     describe('with invalid or no body', function() {
       it('should return a 400 status', done => {
         request.post(`${url}/api/signup`)
-        .send({})
-        .end((err, res) => {
-          expect(res.status).toEqual(400);
-          done();
-        })
-      })
-    })
+          .send({})
+          .end((err, res) => {
+            expect(res.status).toEqual(400);
+            done();
+          });
+      });
+    });
   });
 
 
@@ -73,41 +71,41 @@ describe('Authorization Routes', function() {
       beforeEach( done => {
         let user = new User(exampleUser);
         user.generatePasswordHash(exampleUser.password)
-        .then( user => user.save())
-        .then( user => {
-          this.tempUser = user;
-          done();
-        })
-        .catch(done);
+          .then( user => user.save())
+          .then( user => {
+            this.tempUser = user;
+            done();
+          })
+          .catch(done);
       });
 
       afterEach( done => {
         User.remove({})
-        .then( () => done())
-        .catch(done)
+          .then( () => done())
+          .catch(done);
       });
 
       it('should return a token', done => {
         //for signin we need to have username and password as base64 string on authorization header
         request.get(`${url}/api/signin`)
-        .auth('exampleuser', '1234')
-        .end((err, res) => {
-          if (err) return done(err);
-          expect(res.status).toEqual(200);
-          expect(typeof res.text).toEqual('string');
-          done();
-        });
+          .auth('exampleuser', '1234')
+          .end((err, res) => {
+            if (err) return done(err);
+            expect(res.status).toEqual(200);
+            expect(typeof res.text).toEqual('string');
+            done();
+          });
       });
     });
     
     describe('with an invalid un/password', function() {
       it('should return a 401 status', done => {
         request.get(`${url}/api/signin`)
-        .end((err, res) => {
-          expect(res.status).toEqual(401);
-          done();
+          .end((err, res) => {
+            expect(res.status).toEqual(401);
+            done();
+          });
       });
-     });
     });
   });
 });
--- lab-katy/lib/error-middleware.js
+++ lab-katy/lib/error-middleware.js
@@ -22,4 +22,4 @@ module.exports = function(err, req, res, next) {
   err = createError(500, err.message);
   res.status(err.status).send(err.name);
   next();
-}
+};
